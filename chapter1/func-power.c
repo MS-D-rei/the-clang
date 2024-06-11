2a3ede855fe8267ca8027e87fe60612f375df7a2
MS_D_rei <msmdmission1111@gmail.com>
2024-06-11 23:06:36 +0900

    Add: power2 func to show diff way in func-power.c

--- chapter1/func-power.c
+++ chapter1/func-power.c
@@ -2,22 +2,36 @@
 
 /* Function prototype (Forward declaration) */
 /* This can be `int power(int, int)` */
-int power(int number, int power);
+int power1(int number, int power);
+int power2(int number, int power);
 
 int main() {
     int i;
 
     for (i = 0; i < 10; ++i) {
-        printf("%d %d %d\n", i, power(2, i), power(-3, i));
+        printf("%d %d %d\n", i, power1(2, i), power1(-3, i));
+        printf("%d %d %d\n", i, power2(2, i), power2(-3, i));
     }
 
     return 0;
 }
 
-int power(int base, int power) {
+/* power1: rase base to n-th power; n >= 0 */
+int power1(int base, int power) {
     int output = 1;
 
     for (int i = 1; i <= power; ++i) {
+        output = output * base;
+    }
+
+    return output;
+}
+
+/* In this way, no need to use `i` */
+int power2(int base, int power) {
+    int output = 1;
+
+    for (; power > 0; --power) {
         output = output * base;
     }
 
